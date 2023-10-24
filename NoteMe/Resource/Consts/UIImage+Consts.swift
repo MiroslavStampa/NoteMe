fbf4e2036a194565f2a6bfdf175707e9db4b541d
MiroslavStampa <mirik@tut.by>
2023-10-24 20:52:36 +0300

    Added snap kit, added constans

---
+++ NoteMe/Resource/Consts/UIImage+Consts.swift
@@ -0,0 +1,18 @@
+//
+//  UIImage+Consts.swift
+//  NoteMe
+//
+//  Created by Miroslav Martynovich on 24.10.23.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    //MARK: - General
+    enum General{
+        
+        static let logo: UIImage = .init(named: "logo")!
+        
+    }
+}
