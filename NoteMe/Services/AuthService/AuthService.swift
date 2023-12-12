1f48f8543141e278885e31b1430dbf16511a2eeb
MiroslavStampa <mirik@tut.by>
2023-12-12 18:55:36 +0300

    modified auth service

--- NoteMe/Services/AuthService/AuthService.swift
+++ NoteMe/Services/AuthService/AuthService.swift
@@ -14,6 +14,17 @@ final class AuthService {
     }
     func singIn(email: String, password: String, completion: @escaping (Bool) -> Void) {
         firebase.signIn(withEmail: email, password: password) { result,error in
+            print(error?.localizedDescription)
+            completion(error == nil)
+        }
+    }
+
+    func createUser(email:String,
+                    password: String,
+                    completion: @escaping (Bool) -> Void){
+        firebase.createUser(withEmail: email, password: password) { result, error in
+            print(result)
+            print(error)
             completion(error == nil)
         }
     }
